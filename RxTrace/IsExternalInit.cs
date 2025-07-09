164a6aed9dcc31552579f650f82f81f4e6e8fc59
Johann Dirry <johann.dirry@microsea.at>
2025-07-09 14:54:39 +0200

    adding support for older .NET versions

---
+++ RxTrace/IsExternalInit.cs
@@ -0,0 +1,12 @@
+﻿#if !(NET5_0_OR_GREATER)
+using System.ComponentModel;
+
+namespace System.Runtime.CompilerServices
+{
+    /// <summary>
+    /// Dummy class that fixes CS0518
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    internal static class IsExternalInit { }
+}
+#endif
